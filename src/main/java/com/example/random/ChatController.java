12a8a76ae39d8a668afafb1f4844e5a8097ce0af
sunguk <kkcc4104@naver.com>
2025-02-11 18:36:58 +0900

    commit

---
+++ src/main/java/com/example/random/ChatController.java
@@ -0,0 +1,33 @@
+package com.example.random;
+
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ChatController {
+
+    private final SimpMessageSendingOperations template;
+
+    @GetMapping("/chat/{id}")
+    public ResponseEntity<List<ChatMessage>> getChatMessages(@PathVariable Long id) {
+        ChatMessage test = new ChatMessage(1L, "test", "test");
+        return ResponseEntity.ok().body(List.of(test));
+    }
+
+    @MessageMapping("/message")
+    public ResponseEntity<Void> receiveMessage(@RequestBody ChatMessage chat) {
+        // 메시지를 해당 채팅방 구독자들에게 전송
+        template.convertAndSend("/sub/chatroom/1", chat);
+        return ResponseEntity.ok().build();
+    }
+}
